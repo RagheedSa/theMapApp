f4e5544812ac9418e0a058cf6987dacb1bb64f08
Ragheed <Ragheed.sa@hotmail.com>
2021-02-24 15:58:27 +0300

    new

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+
+  export class Tab2Page {
+
+    constructor(private authService: AuthenticationService, private router: Router) {}
+   
+    async logout() {
+      await this.authService.logout();
+      this.router.navigateByUrl('/', { replaceUrl: true });
+    }
+}
